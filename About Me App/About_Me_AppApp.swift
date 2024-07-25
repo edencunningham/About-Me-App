c9cefae7c32c588a81b0d01a8f321ff820261233
edencunningham <eden85sc@gmail.com>
2024-07-25 10:39:07 -0400

    Initial Commit

---
+++ About Me App/About_Me_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_AppApp.swift
+//  About Me App
+//
+//  Created by Eden Cunningham on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct About_Me_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
