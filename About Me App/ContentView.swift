6dac6a7480d9072d0e6f866353b3cbbf179a1f68
edencunningham <eden85sc@gmail.com>
2024-07-26 12:53:59 -0400

    light green background

--- About Me App/ContentView.swift
+++ About Me App/ContentView.swift
@@ -9,16 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color("Light Yellow").ignoresSafeArea()
+            
+            VStack(){
+                    VStack {
+                            ZStack {
+              
+                                Image("Environment").aspectRatio(contentMode: .fit).frame(width: 1, height: 1).padding(60)
+                                Text("Eden Cunningham").fontWeight(.bold).multilineTextAlignment(.center)
+                            }
+                        Image("Me")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 250, height: 400)
+            
+                        Text("I enjoy gardening, coding, and creativity!\n I want to help the environment with tech.").multilineTextAlignment(.trailing).lineSpacing(8.0).background(Color("Light Green"))
+                    }
+            }
         }
         .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
+
+
